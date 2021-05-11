5edfba1bf877e73bf053b3ff2af95bf991462be5
Jono L <contact@cityeditionstudio.co.uk>
2021-05-11 12:49:24 +0100

    end of vid 18

--- wp-content/themes/nomad-sun/template-parts/content.php
+++ wp-content/themes/nomad-sun/template-parts/content.php
@@ -13,29 +13,30 @@
 
 
 
-	<!-- here we used an inline style attributed to attach our hero image as a css background directly onto the element -->
-	<section class="hero" style="
+	<!-- here we used an inline style attributed to attach our hero image as a css background directly onto the element. 
+	And the section have a min height of 100vh, cover and centre background etc -->
+	<section class="hero min-vh-100 cover bg-center flex items-center justify-center" style="
 	
 		<?php if(get_field('hero_image') ): ?>
     		background-image: url(<?php the_field('hero_image'); ?>);
 		<?php endif; ?>
 
 	">
-	
-		<div class="hero-content">
+		<!-- make text white and centre it -->
+		<div class="hero-content white tc">
 		<!-- location title -->
 
-			<h1><?php the_title(); ?></h1>
+			<h1 class="hero-heading archivo mt0 mb3 bold ma0 ttu"><?php the_title(); ?></h1>
 		<!-- subhead -->
 
 		<?php if(get_field('subhead') ): ?>
-    		<p><?php the_field('subhead'); ?></p>
+    		<p class="hero-subhead tenor mb6 mt0 white ttu"><?php the_field('subhead'); ?></p>
 		<?php endif; ?>
 
 		<!-- date -->
 		<?php if(get_field('date') ): ?>
 			
-			<p>
+			<p class="archivo f6 bold white ttu ma0 tracked">
 			<?php
 			// here we convert date into a format php can work with and then we format it to be readable
 			echo date("F Y", strtotime(get_field('date')));
