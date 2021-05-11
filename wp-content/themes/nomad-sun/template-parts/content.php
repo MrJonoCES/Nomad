8ef5d8a7c6819370238b2232c043cc5ff03d696c
Jono L <contact@cityeditionstudio.co.uk>
2021-05-11 10:18:48 +0100

    plugins and post format edits

--- wp-content/themes/nomad-sun/template-parts/content.php
+++ wp-content/themes/nomad-sun/template-parts/content.php
@@ -10,54 +10,36 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				nomad_sun_posted_on();
-				nomad_sun_posted_by();
-				?>
-			</div><!-- .entry-meta -->
+	<!-- hero needs BG image -->
+	<section class="hero">
+		<div class="hero-content">
+		<!-- location title -->
+
+			<h1><?php the_title(); ?></h1>
+		<!-- subhead -->
+
+		<?php if( get_field('subhead') ): ?>
+    		<p><?php the_field('subhead'); ?></p>
 		<?php endif; ?>
+
+		<!-- date -->
+		<?php if( get_field('date') ): ?>
+    		<p><?php the_field('date'); ?></p>
+		<?php endif; ?>
+
+
+		</div>
+	</section>
+
+	<header class="entry-header">
 	</header><!-- .entry-header -->
 
-	<?php nomad_sun_post_thumbnail(); ?>
 
 	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'nomad-sun' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'nomad-sun' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php nomad_sun_entry_footer(); ?>
+		<?php //nomad_sun_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
