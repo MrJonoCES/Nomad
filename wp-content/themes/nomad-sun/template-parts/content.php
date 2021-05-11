05ff2b6daeae7b7d7a7223e740c258dd3b880cd2
Jono L <contact@cityeditionstudio.co.uk>
2021-05-11 12:55:49 +0100

    end of vid 18

--- wp-content/themes/nomad-sun/template-parts/content.php
+++ wp-content/themes/nomad-sun/template-parts/content.php
@@ -11,44 +11,6 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-
-
-	<!-- here we used an inline style attributed to attach our hero image as a css background directly onto the element. 
-	And the section have a min height of 100vh, cover and centre background etc -->
-	<section class="hero min-vh-100 cover bg-center flex items-center justify-center" style="
-	
-		<?php if(get_field('hero_image') ): ?>
-    		background-image: url(<?php the_field('hero_image'); ?>);
-		<?php endif; ?>
-
-	">
-		<!-- make text white and centre it -->
-		<div class="hero-content white tc">
-		<!-- location title -->
-
-			<h1 class="hero-heading archivo mt0 mb3 bold ma0 ttu"><?php the_title(); ?></h1>
-		<!-- subhead -->
-
-		<?php if(get_field('subhead') ): ?>
-    		<p class="hero-subhead tenor mb6 mt0 white ttu"><?php the_field('subhead'); ?></p>
-		<?php endif; ?>
-
-		<!-- date -->
-		<?php if(get_field('date') ): ?>
-			
-			<p class="archivo f6 bold white ttu ma0 tracked">
-			<?php
-			// here we convert date into a format php can work with and then we format it to be readable
-			echo date("F Y", strtotime(get_field('date')));
-			?>
-			</p>
-
-		<?php endif; ?>
-
-
-		</div>
-	</section>
-
 	<header class="entry-header">
 	</header><!-- .entry-header -->
 
