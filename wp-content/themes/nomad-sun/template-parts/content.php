1ee0df9d3546fe1e7bfb795a1aaa09aebde23972
Jono L <contact@cityeditionstudio.co.uk>
2021-06-08 14:30:09 +0100

    end of video 6

--- wp-content/themes/nomad-sun/template-parts/content.php
+++ wp-content/themes/nomad-sun/template-parts/content.php
@@ -29,24 +29,27 @@
     				background-image: url(<?php the_field('hero_image'); ?>);
 					<?php endif; ?>"></div>
 
-					<!-- our content -->
+					<!-- our header content -->
 					<!-- by default we'll make our header content block have 100% width
 					and then we change it to 40% at large screen sizes -->
-					<div class="w-100 w-40-l flex items-center justify-center ph4">
+					<div class="w-100 w-40-l flex items-center justify-center ph3 ph4-l relative">
 						
-						<div class="tc"><!-- a single div for the flex to work on -->
-						<p class="f6 b archivo mt0 mb5 ttu tracked">
+						<div class="tc"> <!-- here we use absolute positioning for larger screens to put the date in the top centre -->
+						<p class="f6 b archivo mt0 mb5 mb0-l ttu tracked absolute-l top-0-l left-0-l w-100-l pt4">
 						<?php echo date("F Y", strtotime(get_field('date')));?>
+						<span class="line mt4"></span>
 						</p>
 						<!-- standard wordpress data, not an ACF -->
-						<h1 class="f1 archivo mt0 mb3 ttu">
+						<!-- we change the type scale for larger screens and squish the line height a bit because of the bigger font size -->
+						<h1 class="f2 f1-l archivo mt0 mb4 ttu lh-title">
 						<?php the_title(); ?>
 						</h1>
-						<p class="f1 tenor mt0 mb3 ttu">
+						
+						<p class="f2 f1-l tenor mt0 mb4 mb5-l ttu lh-title">
 						<?php the_field('subhead'); ?>
 						<!-- specific to this component -->
 						</p>
-						<p class="f4 cardo i measure center">
+						<p class="f4 cardo i measure center mv0">
 						<?php the_sub_field('header_intro'); ?>
 						</p>
 						</div>
