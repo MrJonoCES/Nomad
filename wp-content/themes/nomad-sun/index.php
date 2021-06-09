9e19209445406dbe8417ff1e4c2a59787f20a627
Jono L <contact@cityeditionstudio.co.uk>
2021-06-09 19:18:15 +0100

    Vid 29

--- wp-content/themes/nomad-sun/index.php
+++ wp-content/themes/nomad-sun/index.php
@@ -14,46 +14,24 @@
 
 get_header();
 ?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<?php
 
-	<main id="primary" class="site-main">
+				$query = new WP_Query('posts_per_page=1&orderby=rand');
+				if ($query->have_posts()): while ($query->have_posts()): $query->the_post();
+				// get the id for our current post and store it in a variable
+				$hero_post = get_the_ID();
+				get_template_part('template-parts/content-hero');
+				endwhile; endif;
 
-		<?php
-		if ( have_posts() ) :
+				get_template_part('template-parts/content-about');
+				get_template_part('template-parts/content-navigate');
+				get_template_part('template-parts/content-bio');
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				get_template_part( 'template-parts/content-hero' );
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
-
+			?>
+		</main><!-- #main -->
+	</div>
 <?php
 // get_sidebar();
 get_footer();
